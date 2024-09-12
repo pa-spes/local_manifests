04a3624b4d9f14e7082d825a9a17e5b96629965a
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-09-12 08:09:34 +0530

    Revert "vendor: drop patch ims.apk commit"
    This reverts commit d3a12189d057a0984a6b201993593f8359f5e7e3.

--- crave.sh
+++ crave.sh
@@ -8,7 +8,7 @@ rm -rf */xiaomi
 git config --global user.name "Kajal4414"
 git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
 git clone -b arrow-13.1 https://github.com/scas-projects/android_device_xiaomi_spes.git device/xiaomi/spes
-git clone -b arrow-13.1 https://github.com/scas-projects/android_vendor_xiaomi_spes.git vendor/xiaomi/spes && (cd vendor/xiaomi/spes && git reset --hard HEAD~1)
+git clone -b arrow-13.1 https://github.com/scas-projects/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
 git clone -b arrow-13.1 --depth 1 https://github.com/scas-projects/android_kernel_xiaomi_spes.git kernel/xiaomi/spes
 
 [ ! -d "hardware/xiaomi" ] && git clone -b arrow-13.1 https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git hardware/xiaomi
