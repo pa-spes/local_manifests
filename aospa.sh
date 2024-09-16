72c65cd9ca88ff021743ec66e34e3cab2d825af2
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-09-16 10:25:39 +0530

    frameworks/av
    av: support per-app volume [1/3]
    audio: Check MODIFY_AUDIO_ROUTING permission for per-app volume

--- aospa.sh
+++ aospa.sh
@@ -6,6 +6,7 @@ repo init -u https://github.com/AOSPA/manifest -b uvite
 
 git config --global user.name "Kajal4414"
 git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
+(cd frameworks/av && git fetch https://github.com/pa-spes/android_frameworks_av && git cherry-pick d41b1dc 5e50b0d || git cherry-pick --abort)
 (cd vendor/aospa && git fetch https://github.com/pa-spes/android_vendor_aospa && git cherry-pick b831e2d e62ac43 d6757b7 dcd4364 || git cherry-pick --abort)
 
 ./rom-build.sh spes -i -v beta -t user
