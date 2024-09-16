27c514c14c5fc65c9598e6ea3d5c59f6ae1be07e
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-09-16 18:44:28 +0530

    temp skip some cp
    https://github.com/pa-spes/android_system_core/commit/3c30966caa3ea653c532d686cadac4b9c2a5b129
    error: system/core/llkd/Android.bp:54:20: unrecognized property "product_variables.eng.init_rc"
    
    https://github.com/pa-spes/android_vendor_aospa/commit/e62ac433208ed62aa1d508b5498e7e5be113d6cc
    error: hooks is different in /tmp/src/android/.repo/projects/device/xiaomi/sm6225-common.git vs /tmp/src/android/.repo/project-objects/pa-spes

--- aospa.sh
+++ aospa.sh
@@ -20,10 +20,10 @@ repo init -u https://github.com/AOSPA/manifest -b uvite && /opt/crave/resync.sh
 
 echo -e "${BLUE}Patching AOSPA Sources...${NC}"
 cherry_pick frameworks/av https://github.com/pa-spes/android_frameworks_av d41b1dc 5e50b0d
-cherry_pick frameworks/base https://github.com/pa-spes/android_frameworks_base ed52cc6^..d742321
-cherry_pick system/core https://github.com/pa-spes/android_system_core d64258a^..e1872bf
-cherry_pick vendor/aospa https://github.com/pa-spes/android_vendor_aospa b831e2d^..02f09b9
-cherry_pick packages/apps/Settings https://github.com/pa-spes/android_packages_apps_Settings b7851d1^..bcb96e5
+cherry_pick frameworks/base https://github.com/pa-spes/android_frameworks_base ed52cc6 abced6b 0a706f0 836019b c9d43cf 32adc68 d742321
+cherry_pick system/core https://github.com/pa-spes/android_system_core d64258a 378111f e1872bf
+cherry_pick vendor/aospa https://github.com/pa-spes/android_vendor_aospa b831e2d d6757b7 dcd4364 02f09b9
+cherry_pick packages/apps/Settings https://github.com/pa-spes/android_packages_apps_Settings b7851d1 f3fd662 bcb96e5
 
 echo -e "${BLUE}Starting Build...${NC}"
 ./rom-build.sh spes -i -v beta -t user
