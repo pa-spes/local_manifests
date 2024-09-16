5d0c3a9a6734db3ba448a168551f3fd65fba8e5e
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-09-16 19:03:25 +0530

    fix build/soong
    should fix this
    error: system/core/llkd/Android.bp:54:20: unrecognized property "product_variables.eng.init_rc"

--- aospa.sh
+++ aospa.sh
@@ -19,9 +19,10 @@ git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
 repo init -u https://github.com/AOSPA/manifest -b uvite && /opt/crave/resync.sh
 
 echo -e "${BLUE}Patching AOSPA Sources...${NC}"
+cherry_pick build/soong https://github.com/pa-spes/android_build_soong ba2d27d
 cherry_pick frameworks/av https://github.com/pa-spes/android_frameworks_av d41b1dc 5e50b0d
 cherry_pick frameworks/base https://github.com/pa-spes/android_frameworks_base ed52cc6 abced6b 0a706f0 836019b c9d43cf 32adc68 d742321
-cherry_pick system/core https://github.com/pa-spes/android_system_core d64258a 378111f e1872bf
+cherry_pick system/core https://github.com/pa-spes/android_system_core d64258a 378111f 3c30966 e1872bf
 cherry_pick vendor/aospa https://github.com/pa-spes/android_vendor_aospa b831e2d d6757b7 dcd4364 02f09b9
 cherry_pick packages/apps/Settings https://github.com/pa-spes/android_packages_apps_Settings b7851d1 f3fd662 bcb96e5
 
