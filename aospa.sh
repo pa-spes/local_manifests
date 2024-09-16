b6e5ce6d2abf2db29dfe1e6ee85285ca0f9489a7
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-09-16 10:29:07 +0530

    packages/apps/Settings
    Settings: Let app lock use face unlock
    Settings: Introduce App Lock [2/4]
    Settings: support per-app volume [3/3]

--- aospa.sh
+++ aospa.sh
@@ -7,6 +7,7 @@ repo init -u https://github.com/AOSPA/manifest -b uvite
 git config --global user.name "Kajal4414"
 git config --global user.email "81718060+Kajal4414@users.noreply.github.com"
 (cd frameworks/av && git fetch https://github.com/pa-spes/android_frameworks_av && git cherry-pick d41b1dc 5e50b0d || git cherry-pick --abort)
+(cd packages/apps/Settings && git fetch https://github.com/pa-spes/android_packages_apps_Settings && git cherry-pick b7851d1 f3fd662 bcb96e5 || git cherry-pick --abort)
 (cd vendor/aospa && git fetch https://github.com/pa-spes/android_vendor_aospa && git cherry-pick b831e2d e62ac43 d6757b7 dcd4364 || git cherry-pick --abort)
 
 ./rom-build.sh spes -i -v beta -t user
